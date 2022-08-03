5363458a7271ef70ad6a1c10c18119635f1b69dd
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 22:35:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("Please enter your integer: ");
+        a = int.Parse(Console.ReadLine());
+        for (b = 1; b <= a; b++)
+        {
+            if (a % b == 0)
+            {
+                Console.WriteLine(b + " is a factor of " + a);
+            }
+        }
+        Console.ReadLine();
+
+
+
+    }
+}
